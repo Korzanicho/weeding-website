77c4354f613813d112e7e1a888b625a958f1c871
Adrian Korzan <adrian.korzan@gmail.com>
2025-05-20 14:04:56 +0200

    feat: auth for photos page

--- app.js
+++ app.js
@@ -2,8 +2,17 @@
 const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
+require('dotenv').config();
+const session = require('express-session');
 
 const app = express();
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { httpOnly: true } // cookie tylko dla serwera
+}));
 
 // Middleware
 app.use(helmet({
@@ -11,6 +20,7 @@ app.use(helmet({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 // Routing
 const indexRoutes = require('./routes/index');
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,31 @@
 const express = require('express');
 const router = express.Router();
 const viewController = require('../controllers/viewController');
+const authMiddleware = require('../middleware/auth');
 const uploadController = require('../controllers/uploadController');
 
 // Widoki
 router.get('/', viewController.getHome);
 router.get('/history', viewController.getHistory);
-router.get('/photos', viewController.getPhotos);
+router.get('/photos', authMiddleware, viewController.getPhotos);
 
 // Upload
-router.post('/upload', uploadController.uploadImages);
+router.post('/upload', authMiddleware, uploadController.uploadImages);
+
+//login
+router.get('/login', (req, res) => {
+    res.render('login', { title: 'Dostęp do galerii', error: null });
+});
+
+router.post('/login', (req, res) => {
+    console.log(req.body, 'req')
+    const { password } = req.body;
+    if (password === process.env.GALLERY_PASSWORD) {
+        req.session.authorized = true;
+        return res.redirect('/photos');
+    } else {
+        res.render('login', { title: 'Dostęp do galerii', error: 'Nieprawidłowe hasło.' });
+    }
+});
 
 module.exports = router;
