37b192d48b0ffc3b2cb096f3983482517552a176
Adrian Korzan <adrian.korzan@gmail.com>
2025-06-01 10:33:02 +0200

    style: mobile view

--- public/public.js
+++ public/public.js
@@ -1,13 +1,19 @@
-// how many days are left until the 08.06.2025
 function daysUntil(date) {
   const now = new Date();
   const futureDate = new Date(date);
   const diff = futureDate.getTime() - now.getTime();
-  return String(Math.ceil(diff / (1000 * 60 * 60 * 24)));
+  return Math.ceil(diff / (1000 * 60 * 60 * 24));
 }
 
 function putDaysToElement() {
-  document.getElementById("header__counter").innerText = daysUntil("2025-06-08");
+  let daysQuantity = daysUntil("2025-06-08");
+  if (daysQuantity === 0) {
+    document.getElementById("header__counter-text").innerText = "To już dzisiaj!";
+    return;
+  }
+  const prefix = daysQuantity >= 0 ? 'Pozostało' : 'Minęło';
+  if (daysQuantity < 0) daysQuantity = daysQuantity * -1;
+  document.getElementById("header__counter-text").innerText = `${prefix} ${daysQuantity} dni!`;
 }
 
 putDaysToElement();
